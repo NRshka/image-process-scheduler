40eae99ad77675fde81ad55adbdfc63aecb4b43a
Александр Макаров <makarov.alxr@yandex.ru>
2021-07-06 17:17:29 +0700

    method for saving index on disk

--- improsch/processors/deduplication.py
+++ improsch/processors/deduplication.py
@@ -21,10 +21,14 @@ class ImageIndex:
             self.index = faiss.index_factory(feat_dim, "Flat", faiss.METRIC_INNER_PRODUCT)
 
     def add_vectors(self, vectors):
+        # we found cosine similarity using inner product
+        # so vectors shoul be normalized
         faiss.normalize_L2(np.array(vectors))
         self.index.add(vectors)
 
     def find_neighbours(self, vectors):
+        # we found cosine similarity using inner product
+        # so vectors shoul be normalized
         faiss.normalize_L2(np.array(vectors))
         distances, indexes = self.index.search(vectors, 2)
 
@@ -32,6 +36,10 @@ class ImageIndex:
         for i in range(distances.shape[0]):
             neighbours.append((i, indexes[i, 1], distances[i, 1]))
 
+        # maximum values on top
         neighbours.sort(key=lambda x: 1 - x[-1])
 
         return neighbours
+
+    def save_on_disk(self, path: str):
+        faiss.write_index(self.index, path)
