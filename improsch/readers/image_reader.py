2a2202b51093c7ce9837dbea47e36fcc4c650274
Александр Макаров <makarov.alxr@yandex.ru>
2021-07-09 16:10:46 +0700

    fix bug with same name

--- improsch/readers/image_reader.py
+++ improsch/readers/image_reader.py
@@ -13,19 +13,17 @@ def get_image_reader(filter_func: Optional[Callable[[np.ndarray], bool]]):
             filenames = map(lambda x: os.path.join(root, x), files)
 
             for filename in filenames:
-                images = []
 
-                for name in filenames:
-                    try:
-                        img = cv2.imread(name)
-                        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+                try:
+                    img = cv2.imread(filename)
+                    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-                        if not filter_func or filter_func(img):
-                            images.append(img)
-                            imagenames.append(filename)
-                    except Exception:
-                        # TODO alerting
-                        continue
+                    if not filter_func or filter_func(img):
+                        images.append(img)
+                        imagenames.append(filename)
+                except Exception:
+                    # TODO alerting
+                    continue
 
             return images, imagenames
 
